eec22d89c0f24f125734c5289613b1454d988ca1
Marcos Gil <marcos_gilf@hotmail.com>
2016-04-20 13:32:22 +0200

    Created my-shop element and began with the my-datasource component

--- models/items_model.js
+++ models/items_model.js
@@ -6,8 +6,12 @@ let ObjectId = Schema.ObjectId;
 
 // mongoose schema
 let ItemSchema = new Schema({
-	title: String,
-	body: String
+	title : String,
+	body : String,
+	categories : {
+		type: String,
+        enum : ['1', '2', '3']
+    }
 });
 
 module.exports = mongoose.model('Item', ItemSchema);
